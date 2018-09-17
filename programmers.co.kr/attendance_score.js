0660cad004b5a9a14b97b6cd414e46ee1c18549a
jjy0120 <jjy0120@mz.co.kr>
2018-09-17 11:29:54 +0900

    programmers.co.kr 문제 작성

---
+++ programmers.co.kr/attendance_score.js
@@ -0,0 +1,14 @@
+/*
+  지각: L, 결석: P, 출석: A
+  출석: +1, 결석: -1, 지각:0
+  지각 2번 -> 결석 1번
+  결석 3번 -> 0점
+==============================================
+  input : ["AAAPPLAA", "AAPAAAAL", "PALLLLPA"]
+  output: [3, 5, 0]
+*/
+
+function solution(arr) {
+  var answer = [];
+  return answer;
+}
---
+++ programmers.co.kr/recursion.js
@@ -0,0 +1,11 @@
+/*
+  reverse 해도 동일한 값이면 true, 아니면 false 리턴
+  ==============================================
+  1. input : 12321 / output: true
+  2. input : 43212 / output: false
+*/
+
+function solution(n) {
+  var answer = false;
+  return answer;
+}
